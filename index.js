c47ec82d88ecfc2a541d1b1cd126579f36ca205c
Ashton Tobar <+>
2022-05-18 13:20:06 -0500

    commit tree 4714406999c460d9598be9596581fba57247f4a6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activityToday = "go to the office") {
+    return `This Monday, I will ${activityToday}.`;
+};
+
+function wrapAdjective(visFlair = "*") {
+    const descriptionOfYou = function(adjective = "special") {
+        return `You are ${visFlair}${adjective}${visFlair}!`;
+    };
+    return descriptionOfYou;
+}
+// wrapAdjective("%")("a dedicated programmer");
